ace7e7ac386925012d57011f12ef13adb0571fd7
lintao <lintao.zhang@thoughtworks.com>
2020-10-29 16:50:14 +0800

    [Lintao] feat: init TraineeList component

---
+++ src/App/Components/TraineeList/TraineeList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import TraineeApi from '../../../Api/trainee';
+
+class TraineeList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trainees: [],
+    };
+  }
+
+  componentDidMount() {
+    TraineeApi.getUngroupedTrainees().then((r) => {
+      this.setState({ trainees: r });
+    });
+  }
+
+  render() {
+    return (
+      <section className="groups-all-trainee">
+        <h3>学员列表</h3>
+        <p>Trainees: {this.state.trainees}</p>
+      </section>
+    );
+  }
+}
+
+export default TraineeList;
