4a7b1afd44eb2593ea3002e5a9e4cc04d583b52b
lintao <lintao.zhang@thoughtworks.com>
2020-10-29 16:49:16 +0800

    [Lintao] feat: init GroupList component

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import TraineeList from "./Components/TraineeList/TraineeList";
 import Header from "./Components/Header/Header"
 import './App.scss';
+import GroupList from "./Components/GroupList/GroupList";
 
 class App extends Component {
     render() {
         return (
             <div data-testid="app" className="App">
                 <Header/>
+                <GroupList/>
                 <TraineeList/>
             </div>
         );
---
+++ src/App/Components/GroupList/GroupList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class GroupList extends React.Component {
+    render() {
+        return <section>GroupList</section>
+    }
+}
+
+export default GroupList;
